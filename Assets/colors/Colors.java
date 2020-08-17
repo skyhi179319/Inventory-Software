537dfe6d03880a4c3bcd18b4606b3fc017f4ef06
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2020-08-17 14:54:15 -0600

    Updated Colors

--- Assets/colors/Colors.java
+++ Assets/colors/Colors.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public class Colors {
     public static Color info = new Color(242,242,189);
+    public static Color LightLightYellow = new Color(242,242,189);
     public static Color gold = new Color(255, 215, 0);
     public static Color darkgoldenrod = new Color(184,134,11);
     public static Color firebrick = new Color(174,34,34);
